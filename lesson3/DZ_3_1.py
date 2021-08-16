0039d0e12e22306614758611adec5045e2554926
Vladimir <vladimir_ryus@mail.ru>
2021-08-16 14:48:25 +0300

    DZ_3_1

---
+++ lesson3/DZ_3_1.py
@@ -0,0 +1,17 @@
+dictionary = {
+    'one': 'один',
+    'two': 'два',
+    'three': 'три',
+    'four': 'четыре',
+    'five': 'пять',
+    'six': 'шесть',
+    'seven': 'семь',
+    'eight': 'восемь',
+    'nine': 'девять',
+    'ten': 'десять'
+}
+
+def translate(eng):
+    return dictionary.get(eng)
+
+print(translate('nine'))
